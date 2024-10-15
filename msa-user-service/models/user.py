c96a0a672d845718b0cb3de2e674bd0fab1150ef
pq100 <system32bit@naver.com>
2024-10-15 11:19:44 +0900

    user service - sqlite orm 설정

---
+++ msa-user-service/models/user.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user'
+
+    mno = Column(Integer, primary_key=True,
+                 autoincrement=True, index=True)
+    userid = Column(String(18), nullable=False)
+    passwd = Column(String(128), nullable=False)
+    name = Column(String(15), nullable=False)
+    email = Column(String(50), nullable=False)
+    regdate = Column(String(20), default=datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
--- msa-user-service/routes/user.py
+++ msa-user-service/routes/user.py
@@ -1,15 +1,14 @@
 from fastapi import APIRouter
-from pydantic import BaseModel
+from fastapi.params import Depends
+from sqlalchemy.orm import Session
+
+from schema.user import User, UserBase
+from service.database import get_db
 
 router = APIRouter()
 
-class User(BaseModel):
-    userid: str
-    passwd: str
-    name: str
-    email: str
-@router.post('/user')
-async def new_user(user: User):
+@router.post('/user', response_model=User)
+async def new_user(user: UserBase, db: Session=Depends(get_db)):
     print(user)
 
     return {'msg': 'ok'}
