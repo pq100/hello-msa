a963d2f6c18e4c7cd082f08bbb851bd63869189e
pq100 <system32bit@naver.com>
2024-10-17 15:58:37 +0900

    frontend - 로그인 후 토큰 확인, 세션 스토리지에 토큰 저장

--- msa-frontend/public/js/userlogin.js
+++ msa-frontend/public/js/userlogin.js
@@ -10,24 +10,30 @@ loginbtn.addEventListener('click', () => {
     });
     console.log(jsondata);
 
-    const res = fetch('http://127.0.0.1:8000/userlogin',
-        {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(jsondata)
-        })
-
+    fetch('http://127.0.0.1:8000/userlogin', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(jsondata)
+    })
         .then(res => {
-            if (res.status === 401) { alert('회원 로그인 실패!!'); }
-            else if (res.status === 200) { alert('회원 로그인 성공!!'); }
-
-        }).catch((error) => {
+            if (res.status === 401) {
+                alert('회원 로그인 실패!!');
+            } else if (res.status === 200) {
+                return res.json();
+            } else {
+                alert('로그인 토큰 확인 불가!!');
+            }
+        })
+        .then(data => {
+            // console.log(data.access_token);     // 토큰 확인
+            // localStorage.setItem('token', data.access_token);   // 토큰 저장
+            sessionStorage.setItem('token', data.access_token);   // 토큰 저장
+            alert('회원 로그인 성공!!');
+        })
+        .catch((error) => {
             alert('회원 로그인 오류 발생!!');
         });
-
 });
-
-
